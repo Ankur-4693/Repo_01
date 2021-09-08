06a349e6a9da4bf8e8d2674bdf92f094f82697a3
Ankur-4693 <45304161+Ankur-4693@users.noreply.github.com>
2021-09-08 08:04:17 +0530

    merge commit
    merge commit

--- src/com/bs/Test.java
+++ src/com/bs/Test.java
@@ -3,10 +3,7 @@ package com.bs;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("feature1 branch");
-<<<<<<< HEAD
-		System.out.println("good morning feature1 bye");
-=======
-		System.out.println("good morning master hii");
->>>>>>> 6e310a9c00c8974244a994586b9f31f27f88f676
+System.out.println("good morning feature1 bye");
+System.out.println("good morning master hii");
 	}
 }
